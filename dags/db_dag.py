f16a6bda1b694e94e806d3a49280665af65862a1
Aya Mamdouh <aya.mamdouh10@icloud.com>
2026-04-21 22:48:09 +0200

    Revert "auto-repair: plan_ep_045 (missing_column)"
    This reverts commit e49e65ffb87dc1a00921135fb7dafd5a4bed4629.

--- dags/db_dag.py
+++ dags/db_dag.py
@@ -132,8 +132,6 @@ with DAG(
     # BEGIN PRECHECKS
     # Precheck tasks can be added here for validation before main pipeline
     check_for_required_columns
-    nonexistent_column
-    nonexistent_column
     # END PRECHECKS
 
     t_read = PythonOperator(
